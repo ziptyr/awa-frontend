ca007cf6c1d03ffd0afff6fab9cef184e6ed23a9
ziptyr <koulu@debian.localdomain>
2021-11-19 13:14:02 +0200

    Restaurants.js is now the listing page of all restaurants

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,48 +1,13 @@
 import React from 'react'
 import styles from './Restaurants.module.css';
-import { Link } from 'react-router-dom';
-
+import Restaurant from './Restaurant';
 
 
 export default function Restaurants(props) {
-    
-    
-  
-    
 
-
-    return (
-        <div className={styles.container}>
-               {props.updatedRestaurants.map((data, key) => {
-                   return (
-                    <Link to={data.id } style={{ textDecoration: 'none' }} ><div className={styles.box}>
-                       <div key={data.id}>
-                           <img src={data.image} alt=""/>
-                                <div className={styles.title}>
-                                    {data.name}  
-                                </div>
-                           <div className={styles.restInfo}>
-                                 <div>
-                                    {data.address}
-                                </div>
-                                <div>
-                                    Avoinna: {data.openHrs}
-                                </div>
-                                <div>
-                                    Hinta: {data.price}
-                                </div>
-                                <div>
-                                    Tyyli: {data.type}
-                                </div>
-                            </div>
-                        </div>
-     
-                    </div> 
-                    </Link>
-                    
-                   );
-                     
-               })}
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      {props.restaurants.map((data, key) => <Restaurant key={key} data={data} />)}
+    </div>
+  )
 }
