eb8541fecab7882c05a51b9349fbbb66e8888024
ziptyr <koulu@debian.localdomain>
2021-11-19 13:12:41 +0200

    Restaurant.js is now the individual restaurant page

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,14 +1,35 @@
 import React from 'react'
-import { Outlet, Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import styles from './Restaurant.module.css'
 
-export const Restaurant = () => {
-    return (
-        <div className={styles.container}>
-            <Link to="/restaurants"><h1>Restaurants List</h1></Link>
 
-             {/* <Outlet />  */}
-            
+export default function Restaurant({data}) {
+
+  return (
+    <Link to={data.id } style={{ textDecoration: 'none' }} >
+      <div className={styles.box}>
+        <div key={data.id}>
+          <img src={data.image} alt=""/>
+          <div className={styles.title}>
+            {data.name}
+          </div>
+
+          <div className={styles.restInfo}>
+            <div>
+              {data.address}
+            </div>
+            <div>
+              Avoinna: {data.openHrs}
+            </div>
+            <div>
+              Hinta: {data.price}
+            </div>
+            <div>
+              Tyyli: {data.type}
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </Link>
+  )
 }
