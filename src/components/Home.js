f5c480c9ca4db14d362de1358cbd38a6a32b6362
ziptyr <koulu@debian.localdomain>
2021-11-19 13:14:48 +0200

    Home moved to components and changed to rfc style

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        Food ordering made easy
+      </div>
+
+      <div className={styles.body}>
+      </div>
+    </div>
+  )
+}
