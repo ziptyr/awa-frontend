7efa3078febfb7e5f692455ee5f67fb9def9469f
Aki Peltokangas <v0peak00@students.oamk.fi>
2021-12-01 11:22:27 +0200

    Added support for basic authentication via axios

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,24 +1,55 @@
-import React from 'react'
+import React, { useState} from 'react'
+import axios from 'axios'
 import styles from './SignUp.module.css'
+import Constants from './Constants.json'
+import { useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+    const [ signupProcessState, setSignupProcessState] = useState("idle")
+    const navigate = useNavigate();
     
-    const inputUsername = <form>
-        <label>
-            Name: <br/>
-                <input type="text" name="name" />
-            </label>
-    </form>
+    const handleSignupSubmit = async (event) => {
+        event.preventDefault();
+        setSignupProcessState("processing");
+         
+        try {
+            const result = await axios.post(Constants.API_ADDRESS + '/registerBasic',
+            {
+                username: event.target.username.value,
+                email: event.target.email.value,
+                password: event.target.password.value
+            })
+            console.log(result);
+            setSignupProcessState("signupSuccess")
+            setTimeout(() => {
+                navigate('/login', { replace: true })
+            },1500)
+        } catch (error) {
+            console.log(error);
+            setSignupProcessState("signupFailure");
+        }
+    }
 
-    const inputPassword = <form>
-        <label>
-            Password: <br/>
-                <input type="password" name="name" />
-            </label>
-    </form>
-       
+    let signupUIControls = null;
+    switch(signupProcessState) {
+        case "idle":
+            signupUIControls = <button className={styles.logInBtn} type="submit">Submit</button>
+            break;
 
-    // const inputPassword;
+        case "processing":
+            signupUIControls = <span>Processing...</span>
+            break;
+
+        case "signupSuccess":
+            signupUIControls = <span style={{color: "green"}}>Signup Success</span>
+            break;
+
+        case "signupFailure":
+            signupUIControls = <span style={{color: "red"}}>Error</span>
+            break;
+
+    }
+  
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
@@ -26,24 +57,23 @@ export default function SignUp() {
                     <div className={styles.headerContainer}>
                         <div className={styles.header}>Welcome!</div>
                         <div >Create an Account</div>
-                        
                     </div>
                     <div className={styles.textContainer}>
-                    <form>
-                        <label>
-                            <div>Username:</div>
-                            <input type="text" />
-                        </label>
-                        <label>
-                            <div>Address:</div>
-                            <input type="text" />
-                        </label>
-                        <label>
-                            <div>Password:</div>
-                            <input type="password" />
-                        </label>
+                    <form onSubmit={handleSignupSubmit}>
+                        <div
+                            >Username: <br />
+                            <input type="text" name="username"/>
+                        </div>
+                        <div
+                            >Email: <br />
+                            <input type="text" name="email"/>
+                        </div>
+                        <div>  
+                            Password: <br />
+                            <input type="password" name="password"/>
+                        </div>
                         <div>
-                            <button className={styles.logInBtn} type="submit">Submit</button>
+                            { signupUIControls }
                         </div>
                         </form>
                     </div>
