0d10f9aa35ad01f2c0c5da43a4c3f4de0c5c107c
ziptyr <koulu@debian.localdomain>
2021-12-04 20:39:08 +0200

    style import changed to module.css

--- src/components/Restaurant/RestaurantsManagerManage.js
+++ src/components/Restaurant/RestaurantsManagerManage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './RestarantsManagerManage.js';
+import style from './RestarantsManagerManage.module.css';
 
 export default function RestaurantsManagerManage() {
     return (
