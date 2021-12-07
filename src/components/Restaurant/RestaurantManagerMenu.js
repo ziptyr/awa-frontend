1de9bd5bd63d8e6d5609a981a209472a5c48d651
ziptyr <koulu@debian.localdomain>
2021-12-07 10:18:42 +0200

    initial managers menu pages

---
+++ src/components/Restaurant/RestaurantManagerMenu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './RestaurantManagerMenu.module.css'
+
+export default function RestaurantsManagerMenu() {
+    return (
+        <div>
+            <div className={styles.left}>
+                left
+            </div>
+
+            <div className={styles.right}>
+                right
+            </div>
+        </div>
+    )
+}
