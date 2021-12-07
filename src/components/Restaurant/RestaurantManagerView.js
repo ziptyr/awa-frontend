28e3776b19dba2483b738cbb7a87a1d9c62249b9
ziptyr <koulu@debian.localdomain>
2021-12-08 00:42:51 +0200

    using mapping functions for restaurant managers orders view

--- src/components/Restaurant/RestaurantManagerView.js
+++ src/components/Restaurant/RestaurantManagerView.js
@@ -1,5 +1,6 @@
 import { React, useState} from 'react'
 import {GetRestaurant, GetNewOrders, GetInProgressOrders} from '../Tools';
+import { Link } from 'react-router-dom';
 import styles from './RestaurantManagerView.module.css'
 
 
@@ -9,36 +10,60 @@ export const RestaurantManagerView = ({menuData, restaurants, orders}) => {
     if (restaurant === null) return (<div>No restaurant found</div>);
 
     const newOrders = GetNewOrders(orders);
-    const inProgress = GetInProgressOrders(orders);
+    const inProgressOrders = GetInProgressOrders(orders);
 
     //const menu = GetMenu(menuData, restaurant);
     //if (menu === null) return (<div>No menu found</div>);
 
-    const orderKeyNames = [
-        'Order ID',
-        'Restaurant ID',
-        'Customer',
-        'Order Status',
-        'Date',
-        'Price',
-        'Address'
-    ]
+    const orderPrettyKeys = {
+        'orderId': 'Order ID',
+        'restaurantId': 'Restaurant ID',
+        'username': 'Customer',
+        'orderStatus': 'Order Status',
+        'orderDate': 'Date',
+        'total': 'Price',
+        'deliveryAddress': 'Address'
+    };
+
+    const newOrderKeys = [
+        'orderId',
+        'username',
+        'orderDate',
+        'total',
+        'deliveryAddress'
+    ];
+
+    const inProgressOrderKeys = [
+        'orderId',
+        'username',
+        'orderStatus',
+        'orderDate',
+        'total',
+        'deliveryAddress'
+    ];
+
+    const newOrderPrettyKeys = newOrderKeys.map((k) => orderPrettyKeys[k]);
+    const inProgressOrderPrettyKeys = inProgressOrderKeys.map((k) => orderPrettyKeys[k]);
 
     const MapKeys = ({keys}) => {
-        return (keys.map((name, i) => (
-            <div key={i} className={styles.header}>
-                {name}
-            </div>
-        )))
+        return (
+            keys.map((name, i) => (
+                <div key={i} className={styles.header}>
+                    {name}
+                </div>
+            ))
+        )
     }
 
-    const MapOrderDetails = ({orders}) => {
+    const MapOrderDetails = ({orders, keys}) => {
         return (orders.map((order, orderIndex) =>
-            Object.values(order.details).map((e, detailIndex) => {
+            keys.map((key, keyIndex) => {
                 return (
-                    <div key={orderIndex + '-' + detailIndex}>
-                        <div>{e}</div>
-                    </div>
+                    <Link to={'' + order.details.orderId} className={styles.link}>
+                        <div key={orderIndex + '-' + keyIndex}>
+                            <div>{order.details[key]}</div>
+                        </div>
+                    </Link>
                 )
             })
         ))
@@ -48,14 +73,14 @@ export const RestaurantManagerView = ({menuData, restaurants, orders}) => {
         <div className={styles.container}>
             <h2>New Orders</h2>
             <div className={styles.newOrders}>
-                <MapKeys keys={orderKeyNames} />
-                <MapOrderDetails orders={newOrders} />
+                <MapKeys keys={newOrderPrettyKeys} />
+                <MapOrderDetails keys={newOrderKeys} orders={newOrders} />
             </div>
 
             <h2>In Progress</h2>
-            <div className={styles.newOrders}>
-                <MapKeys keys={orderKeyNames} />
-                <MapOrderDetails orders={inProgress} />
+            <div className={styles.inProgressOrders}>
+                <MapKeys keys={inProgressOrderPrettyKeys} />
+                <MapOrderDetails keys={inProgressOrderKeys} orders={inProgressOrders} />
             </div>
         </div>                  
     )
