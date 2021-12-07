b919d15e9c3be9ebbcfd43fcd65f5cb8d859f5d2
ziptyr <koulu@debian.localdomain>
2021-12-07 10:53:15 +0200

    manager menu: menu items listed with links

--- src/components/Restaurant/RestaurantManagerMenu.js
+++ src/components/Restaurant/RestaurantManagerMenu.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import styles from './RestaurantManagerMenu.module.css'
+import { useParams } from 'react-router';
+import {Link, Outlet} from 'react-router-dom';
+import styles from './RestaurantManagerMenu.module.css';
 
-export default function RestaurantsManagerMenu() {
+export default function RestaurantsManagerMenu({restaurants, menuData}) {
+    //Finding the correct restaurant to display using useParams().
+    const index = useParams();
+
+    const restaurant = restaurants.find(restaurant => restaurant.id === index.id );
+    if ( restaurant == null) {
+        return <div>No matching restaurant</div>
+    }
+
+    //Filtering the correct menu to display using the above function result.
+    const menu = menuData.filter(menu =>
+        menu.restaurant === restaurant.name
+    )
+
+    console.log(menu)
+
     return (
-        <div>
+        <div className={styles.container}>
             <div className={styles.left}>
-                left
+                {menu.map((item, i) =>
+                    <Link key={i} to={item.id}>
+                        <div>
+                            {item.name}
+                        </div>
+                    </Link>
+                )}
             </div>
 
             <div className={styles.right}>
-                right
+                <Outlet />
             </div>
         </div>
     )
