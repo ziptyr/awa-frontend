885c341dacd1dec8993b0e3259e1f0db61b89058
ziptyr <koulu@debian.localdomain>
2021-12-07 21:17:19 +0200

    initial orders listing for manager

--- src/components/Restaurant/RestaurantManagerView.js
+++ src/components/Restaurant/RestaurantManagerView.js
@@ -1,38 +1,61 @@
-import { React, useState, useReducer} from 'react'
-import { useParams } from 'react-router'
-import {GetRestaurant, GetMenu} from '../Tools';
+import { React, useState} from 'react'
+import {GetRestaurant, GetNewOrders, GetInProgressOrders} from '../Tools';
 import styles from './RestaurantManagerView.module.css'
 
 
-export const RestaurantManagerView = ({menuData, restaurants}) => {
+export const RestaurantManagerView = ({menuData, restaurants, orders}) => {
 
     const restaurant = GetRestaurant(restaurants);
     if (restaurant === null) return (<div>No restaurant found</div>);
 
+    const newOrders = GetNewOrders(orders);
+    const inProgress = GetInProgressOrders(orders);
+
     //const menu = GetMenu(menuData, restaurant);
     //if (menu === null) return (<div>No menu found</div>);
 
+    const orderKeyNames = [
+        'Order ID',
+        'Restaurant ID',
+        'Customer',
+        'Order Status',
+        'Date',
+        'Price',
+        'Address'
+    ]
+
+    const MapKeys = ({keys}) => {
+        return (keys.map((name, i) => (
+            <div key={i} className={styles.header}>
+                {name}
+            </div>
+        )))
+    }
+
+    const MapOrderDetails = ({orders}) => {
+        return (orders.map((order, orderIndex) =>
+            Object.values(order.details).map((e, detailIndex) => {
+                return (
+                    <div key={orderIndex + '-' + detailIndex}>
+                        <div>{e}</div>
+                    </div>
+                )
+            })
+        ))
+    }
+
     return (
-        <div className={styles.wrapper}>
-            <div className={styles.container}>
-                <div className={styles.restaurantInfoContainer}>
-                    <div className={styles.restaurantName}>
-                        {restaurant.name}
-                    </div>
-                </div>
+        <div className={styles.container}>
+            <h2>New Orders</h2>
+            <div className={styles.newOrders}>
+                <MapKeys keys={orderKeyNames} />
+                <MapOrderDetails orders={newOrders} />
+            </div>
 
-                <div className={styles.categoriesWrapper}>
-                    <div className={styles.categoriesContainer}>
-                        asdfasdfölkj
-                    </div>
-                </div>
-
-                <div className={styles.menuContainer}>
-                    <div className={styles.menuHeader}>
-                        asd
-                    </div>
-                </div>    
-
+            <h2>In Progress</h2>
+            <div className={styles.newOrders}>
+                <MapKeys keys={orderKeyNames} />
+                <MapOrderDetails orders={inProgress} />
             </div>
         </div>                  
     )
