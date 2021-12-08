48e076c35f38ee5305bf892e069f4467e1806438
Aki Peltokangas <v0peak00@students.oamk.fi>
2021-12-08 16:13:09 +0200

    Added shopping cart functionality via context

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,12 +1,33 @@
-import React, { useState} from 'react'
+import React, { useContext } from 'react'
+import { Context } from './Context';
+import styles from './ShoppingCart.module.css'
 
-export default function ShoppingCart() {
-
-    const [cart, setCart] = useState(null);
+export default function ShoppingCart(props) {
+    
+    const context = useContext(Context);
 
     return (
-        <div>
-            <h1>Shopping Cart</h1>
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <div className={styles.shoppingCartContainer}>
+                    <div className={styles.cartHeader}>
+                        <h1>Shopping Cart</h1>
+                    </div>
+                    <div className={styles.cartInfo}>
+                        <div>{context.getTotal(context.cart)}€</div>
+                            <button onClick={() => context.empty(context.data)}>Empty Cart</button>
+                    </div>
+                        <div className={styles.cartItems}>
+                        {context.cart.map((data) => <div className={styles.cartItemsFlex}>
+                        <div>{data.name}</div>
+                        <span>{data.price}€</span>
+                        <button onClick={() => context.remove(data)}>-</button>
+                        <button onClick={() => context.add(data)}>+</button>
+                        </div>)} 
+                        </div>
+                        
+                    </div>
+                </div>
         </div>
     )
 }
