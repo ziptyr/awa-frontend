a25fbd379e9eb16a18f5d76763d2d00c171519ae
ziptyr <koulu@debian.localdomain>
2021-12-09 20:32:33 +0200

    tab lenght = 2

--- src/components/Restaurant/RestaurantManager.js
+++ src/components/Restaurant/RestaurantManager.js
@@ -4,59 +4,46 @@ import styles from './RestaurantManager.module.css'
 
 
 export default function RestaurantManager({data}) {
-          //<img src={data.image} alt=""/>
-          /*<div alt={data.image} style={{
-            backgroundColor: "lightblue",
-            backgroundImage: "url(" + data.image + ")",
-            textAlign: "right",
-            width: "225px",
-            height: "225px"
-          }}>*/
 
-  console.log(data)
-
-  return (
-    <div className={styles.box}>
-      <div key={data.id} style={{position: 'relative'}}>
-        <Link to={'' + data.restaurantId} style={{ textDecoration: 'none' }} >
-          <img src={data.image} alt={data.image} width="225px" />
-        </Link>
-
-        <Link to={'manage/' + data.restaurantId}>
-          <button className={styles.buttonEdit}>
-            E
-          </button>
-        </Link>
-
-        <Link to={'' + data.restaurantId}>
-          <div className={styles.title}>
-            {data.restaurantName}
-          </div>
-
-          <div className={styles.marginDiv} />
-        </Link>
-
-        <Link to={'menu/' + data.restaurantId}>
-          <button className={styles.buttonMenu}>
-            Menu
-          </button>
-        </Link>
-
-        <Link to={'' + data.restaurantId}>
-          <div className={styles.marginDiv} />
-
-          <div className={styles.restInfo}>
-            <div>
-              Orders: X
-              <br />
-              New Orders: Y
+    return (
+        <div className={styles.box}>
+            <div key={data.id} style={{position: 'relative'}}>
+                <Link to={'' + data.restaurantId} style={{ textDecoration: 'none' }} >
+                    <img src={data.image} alt={data.image} width="225px" />
+                </Link>
+        
+                <Link to={'manage/' + data.restaurantId}>
+                    <button className={styles.buttonEdit}>
+                        E
+                    </button>
+                </Link>
+        
+                <Link to={'' + data.restaurantId}>
+                    <div className={styles.title}>
+                        {data.restaurantName}
+                    </div>
+          
+                    <div className={styles.marginDiv} />
+                </Link>
+        
+                <Link to={'menu/' + data.restaurantId}>
+                    <button className={styles.buttonMenu}>
+                        Menu
+                    </button>
+                </Link>
+        
+                <Link to={'' + data.restaurantId}>
+                    <div className={styles.marginDiv} />
+          
+                    <div className={styles.restInfo}>
+                        <div>
+                            Orders: X
+                            <br />
+                            New Orders: Y
+                        </div>
+                    </div>
+                </Link>
             </div>
-          </div>
-        </Link>
-      </div>
-    </div>
-  )
+        </div>
+    )
 }
-
-/*
-*/
