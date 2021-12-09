e12074f912748df9c9ea5cf4e1cb062cddb8c9eb
ziptyr <koulu@debian.localdomain>
2021-12-09 20:24:14 +0200

    RestaurantsManager.js removed axios

--- src/components/Restaurant/RestaurantsManager.js
+++ src/components/Restaurant/RestaurantsManager.js
@@ -1,38 +1,10 @@
-import React, {useState, useEffect, useContext} from 'react'
+import React from 'react'
 import styles from './RestaurantsManager.module.css';
 import RestaurantManager from './RestaurantManager';
 import RestaurantManagerAdd from './RestaurantManagerAdd';
-import { Context } from '../Context'
-
-const axios = require('axios').default;
-const HEROKU = 'https://awa-2021-t35.herokuapp.com';
-const restaurantPath = '/manager/restaurants';
 
 
-export default function Restaurants() {
-
-    const [restaurants, setRestaurants] = useState([]);
-    const context = useContext(Context);
-
-    const AXIOS_HEADERS = {headers: {'Authorization': 'Bearer ' + context.userJWT}};
-
-    useEffect(() => {
-        axios.get(HEROKU + restaurantPath, AXIOS_HEADERS)
-            .then(function (response) {
-            // handle success
-            //console.log(response);
-            //restaurants = response.data;
-            //console.log('RestaurantsManager.js')
-            setRestaurants(response.data)
-        })
-            .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-            .then(function () {
-            // always executed
-        });
-    }, []);
+export default function RestaurantsManager({restaurants}) {
 
     return (
         <div className={styles.container}>
