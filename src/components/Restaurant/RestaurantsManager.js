9059ed981c3421138dd0faac802920ef29897f4b
ziptyr <koulu@debian.localdomain>
2021-12-11 03:08:12 +0200

    use DataProvider

--- src/components/Restaurant/RestaurantsManager.js
+++ src/components/Restaurant/RestaurantsManager.js
@@ -2,14 +2,19 @@ import React from 'react'
 import styles from './RestaurantsManager.module.css';
 import RestaurantManager from './RestaurantManager';
 import RestaurantManagerAdd from './RestaurantManagerAdd';
+import {useData} from '../DataProvider';
 
 
-export default function RestaurantsManager({restaurants}) {
+export default function RestaurantsManager() {
+
+    const {restaurants} = useData();
 
     return (
         <div className={styles.container}>
-            {restaurants.map((data, key) => <RestaurantManager key={key} data={data} />)}
+            {restaurants.map((data, key) => <RestaurantManager data={data} key={key} />)}
             <RestaurantManagerAdd />
         </div>
     )
+
+    return (null)
 }
