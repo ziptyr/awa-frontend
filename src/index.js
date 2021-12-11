f7dfb80b70d868834e7d2fd6c25a8365539e7d3c
ziptyr <koulu@debian.localdomain>
2021-12-11 02:29:50 +0200

    new context provider that handels restaurants

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import DataProvider from './components/DataProvider';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <DataProvider>
+            <App />
+        </DataProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-
