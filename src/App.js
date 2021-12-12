4fd6932d41cef5688ebfdf966a70b636ef7f55dc
ziptyr <koulu@debian.localdomain>
2021-12-12 21:08:48 +0200

    App.js cleaning: classes -> Tools/requestClasses.js, customer fncs copied to Tools/customerFunctions.js

--- src/App.js
+++ src/App.js
@@ -22,9 +22,10 @@ import SignUp from './components/SignUp';
 import Account from './components/Account';
 import ShoppingCart from './components/ShoppingCart'
 import Footer from './components/Footer';
-import Constants from './components/Constants.json';
 
 import {useData} from './components/DataProvider';
+
+import {RequestGet, RequestGetRestaurants, RequestPost, RequestPut} from './Tools/requestClasses';
 
 
 // axios constants
@@ -43,153 +44,8 @@ const menuDataIds = menuData.map( data => {
     return { ...data, id: uuidv4()}
 });
 
+
 function App() {
-
-    //CLASSES
-    class Request {
-        baseUrl;
-        decoder;
-        stateVar;
-        stateVarFnc;
-
-        constructor(stateVar = false, stateVarFnc = false) {
-            if (this.constructor == Request) {
-              throw new Error("Abstract classes can't be instantiated.");
-            }
-
-            this.baseUrl = Constants.API_ADDRESS;
-            this.decoder = jwt;
-            this.stateVar = stateVar;
-            this.stateVarFnc = stateVarFnc;
-        }
-
-        getHeaders(codedJWT) {
-            let axiosHeaders;
-
-            if (this.decoder.decode(codedJWT) == null) axiosHeaders = null;
-            else axiosHeaders = {'headers': {'Authorization': 'Bearer ' + codedJWT}};
-
-            return axiosHeaders;
-        }
-
-        request() {
-            /**
-             * must implement request function
-             */
-
-            throw new Error("Method 'get()' must be implemented.");
-        }
-
-        getStateVar() {
-            return this.stateVar;
-        }
-
-        setStateVar(stateVar) {
-            this.stateVar = stateVar;
-        }
-
-        getStateVarFnc() {
-            return this.stateVarFnc;
-        }
-
-        setStateVarFnc(stateVarFnc) {
-            this.stateVarFnc = stateVarFnc;
-        }
-    }
-
-    class RequestGet extends Request {
-
-        request(codedJWT, apiRoute) {
-            /**
-             * perform axios get request
-             */
-
-            axios.get(this.baseUrl + apiRoute, this.getHeaders(codedJWT))
-                .then((response) => {
-                    console.log('RequestGet: ', response.data);
-                    if (this.stateVarFnc != false) this.stateVarFnc(response.data);
-                })
-                .catch((error) => {
-                    console.log('RequestGet: ', error);
-                })
-                .then(() => {
-                    console.log('RequestGet: ' + this.baseUrl + apiRoute);
-                });
-        }
-    }
-
-    class RequestGetRestaurants extends RequestGet {
-
-        constructor() {
-            super(restaurants, setRestaurants);
-        }
-
-        getRestaurantsApiRoute(codedJWT) {
-            let restaurantsUrl;
-            let decoded = this.decoder.decode(codedJWT);
-
-            if (decoded == null) {
-                restaurantsUrl = '/public/restaurants';
-            } else if (decoded.role === 'CUSTOMER') {
-                restaurantsUrl = '/public/restaurants';
-            } else if (decoded.role === 'MANAGER') {
-                restaurantsUrl = '/manager/restaurants';
-            }
-
-            return restaurantsUrl;
-        }
-
-        request(codedJWT) {
-            super.request(codedJWT, this.getRestaurantsApiRoute(codedJWT));
-        }
-    }
-
-    class RequestPost extends Request {
-
-        request(codedJWT, apiRoute, sendData) {
-            /**
-             * perform axios post request
-             */
-
-            console.log('RequestPost data', sendData);
-
-            axios.post(this.baseUrl + apiRoute, sendData, this.getHeaders(codedJWT))
-                .then((response) => {
-                    console.log('RequestPost: ', response.data);
-                    if (this.stateVarFnc != false) this.stateVarFnc(response.data);
-                })
-                .catch((error) => {
-                    console.log('RequestPost: ', error);
-                })
-                .then(() => {
-                    console.log('RequestPost: ' + this.baseUrl + apiRoute);
-                })
-        }
-    }
-
-    class RequestPut extends Request {
-
-        request(codedJWT, apiRoute, sendData) {
-            /**
-             * perform axios post request
-             */
-
-            console.log('RequestPost data', sendData);
-
-            axios.put(this.baseUrl + apiRoute, sendData, this.getHeaders(codedJWT))
-                .then((response) => {
-                    console.log('RequestPut: ', response.data);
-                    if (this.stateVarFnc != false) this.stateVarFnc(response.data);
-                })
-                .catch((error) => {
-                    console.log('RequestPut: ', error);
-                })
-                .then(() => {
-                    console.log('RequestPut: ' + this.baseUrl + apiRoute);
-                })
-        }
-    }
-    //CLASSES END
 
     //CONSTS
     const {restaurants, setRestaurants} = useData();
@@ -203,7 +59,7 @@ function App() {
     //Decoded JWT using jsonwebtoken.decoder.
     const jwtDecoded = jwt.decode(userJWT);
 
-    const requestGetRestaurants = new RequestGetRestaurants();
+    const requestGetRestaurants = new RequestGetRestaurants(restaurants, setRestaurants);
 
     const requestGetOrders = new RequestGet();
     const requestPutOrders = new RequestPut();
@@ -212,7 +68,7 @@ function App() {
     const requestPostMenu = new RequestPost();
 
     const requestPostRestaurant = new RequestPost();
-    const requestPutRestaurant = new RequestPut(restaurants);
+    const requestPutRestaurant = new RequestPut();
     //CONSTS END
 
     //FUNCTIONS
--- src/components/DataProvider.js
+++ src/components/DataProvider.js
@@ -12,38 +12,12 @@ const jwtFromStorage = window.localStorage.getItem("userJWT");
 
 export default function DataProvider({children}) {
 
-    //CLASSES
-    //CLASSES END
-
     //CONSTANTS
     const [userJWT, setUserJWT] = useState(jwtFromStorage);
     const jwtDecoded = jwt.decode(userJWT);
 
     const [restaurants, setRestaurants] = useState([]);
     //CONSTANTS END
-
-    //FUNCTIONS
-    //function getRestaurant(id) {
-    //    let result = restaurants.find((restaurant) => restaurant.restaurantId == id);
-
-    //    if (typeof result === 'undefined') {
-    //        result = {
-    //            'found': false,
-    //            'restaurantName': '',
-    //            'address': '',
-    //            'opens': '',
-    //            'closes': '',
-    //            'image': '',
-    //            'type': 'Casual',
-    //            'priceLevel': 0
-    //        }
-    //    } else {
-    //        result = {'found': true, ...result};
-    //    }
-
-    //    return result;
-    //}
-    //FUNCTIONS END
 
     React.useEffect(() => {
         function getRestaurantsRoute() {
