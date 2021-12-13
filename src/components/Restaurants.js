b9c2c8b9bc34f3e87fa895ba182daf410019e4a5
Aki Peltokangas <v0peak00@students.oamk.fi>
2021-12-13 13:58:54 +0200

    added requestGetMenu as props

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -5,9 +5,10 @@ import Restaurant from './Restaurant';
 
 export default function Restaurants(props) {
 
+  
   return (
     <div className={styles.container}>
-      {props.restaurants.map((data, key) => <Restaurant key={key} data={data} />)}
+      {props.restaurants.map((data, key) => <Restaurant key={key} data={data} requestGetMenu={data.requestGetMenu} />)}
     </div>
   )
 }
