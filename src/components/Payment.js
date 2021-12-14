7dd68b400c1356df8150e7e3b4d3b8cde3f4b7a0
ziptyr <koulu@debian.localdomain>
2021-12-14 15:33:52 +0200

    header and fixed payment.module import

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+import styles from './Payment.module.css';
 
 export default function Payment() {
     return (
         <div>
+            <div className={styles.header}>
+                header
+            </div>
             payment
         </div>
     )
