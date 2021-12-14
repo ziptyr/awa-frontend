a4aafdef3367a41904d644ef580c9267577e032b
ziptyr <koulu@debian.localdomain>
2021-12-14 20:29:03 +0200

    orderhistory key ids

--- src/components/Account/OrderHistory.js
+++ src/components/Account/OrderHistory.js
@@ -25,22 +25,22 @@ export default function OrderHistory({userJWT}) {
     return (
         <div className={styles.container}>
             <div className={styles.row}>
-                {Object.values(orderKeys).map((val) => {
+                {Object.values(orderKeys).map((val, h) => {
                     return (
-                        <div className={styles.header}>
+                        <div className={styles.header} key={h}>
                             {val}
                         </div>
                     )
                 })}
             </div>
 
-                {requestGetOrders.getStateVar().map((order) => {
+                {requestGetOrders.getStateVar().map((order, i) => {
                     return (
-                        <Link to={'' + order.orderId}>
+                        <Link to={'' + order.orderId} key={i}>
                             <div className={styles.dataRow}>
-                                {Object.keys(orderKeys).map((key) => {
+                                {Object.keys(orderKeys).map((key, a) => {
                                     return (
-                                        <div className={styles.dataCol}>
+                                        <div className={styles.dataCol} key={a}>
                                             {order[key]}
                                         </div>
                                     )
