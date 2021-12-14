eded72d11271ed01395ba1ae3014e67bad8723d5
Aki Peltokangas <v0peak00@students.oamk.fi>
2021-12-14 19:28:09 +0200

    moved card components to Payments.js, using payment to state for rendering components

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -6,7 +6,7 @@ import {useData} from './DataProvider';
 
 
 
-export default function ShoppingCart({requestPostOrder}) {
+export default function ShoppingCart({requestPostOrder, payment, setPayment}) {
     
     const context = useContext(Context);
 
@@ -14,61 +14,9 @@ export default function ShoppingCart({requestPostOrder}) {
 
     const [address, setAddress] = useState('');
 
-    const [card, setCard] = useState('');
-    const [name, setName] = useState('');
-    const [cvc, setCvc] = useState('');
-    
-        const marker = [];
-
-        if(card.length == 16)
-        marker[0] = <span className={styles.greenMarker}></span>
-        else 
-            marker[0] = <span className={styles.redMarker}></span> 
-            
-            if(name.length > 10)
-            marker[1] = <span className={styles.greenMarker}></span>
-            else 
-                marker[1] = <span className={styles.redMarker}></span> 
-            
-                if(cvc.length == 3)
-                marker[2] = <span className={styles.greenMarker}></span>
-                else 
-                    marker[2] = <span className={styles.redMarker}></span> 
-    
-        let order = <>
-            <div className={styles.addText}>
-                <div >Card Number: 
-                    <input
-                    type="text"
-                    value={card}
-                    onChange={ (e) => setCard(e.target.value) } />
-                    {marker[0]}
-                </div>
-                {marker[0]}
-           
-                <div > Cardholder Name: 
-                <input
-                type="text"
-                value={name}
-                onChange={ (e) => setName(e.target.value) } />
-                </div>
-                {marker[1]}
-
-                <div > CVC: 
-                <input className={styles.inputLength}
-                type="text"
-                value={cvc}
-                onChange={ (e) => setCvc(e.target.value) } />                
-                </div>
-                {marker[2]}
-                
-            </div>
-        </>
-
-            
-            if( card.length == 16 && name.length > 10 && cvc.length == 3 )
-            
-                order = <><button className={styles.orderButton}
+   
+        let order;
+        order = <><button className={styles.orderButton}
                     onClick={() => {
             
                     let sendData = {"products": {},"deliveryAddress": address};
@@ -78,13 +26,17 @@ export default function ShoppingCart({requestPostOrder}) {
                             } else {
                                 sendData.products[x.productId] = 1;
                             }
+                            
                         });
                         
                     requestPostOrder.request(userJWT, '/customer/buy', sendData);
+                    context.empty();
+                    setAddress();
+                    setPayment(false);
                 }}>
                     Confirm Order
             </button>
-            <button onClick={() => setCard('')}>Edit Card</button>
+            
             </>
 
 
@@ -118,18 +70,23 @@ export default function ShoppingCart({requestPostOrder}) {
                                                     value={address}
                                                     onChange={ (e) => setAddress(e.target.value) } />
                                             </div>
+                                            {(payment == false ? (
+                                            <>
+                                            <Link to='/payment'>
+                                                <button>
+                                                    payment page
+                                                </button>
+                                            </Link>
+                                            </>)   : null)}
                              
                         </div>
-                        {order}
-                        
+                        {(payment != false ? (
+                            (order)
+                        )   : null)}
                     </div>
                 </div>
 
-                <Link to='/payment'>
-                    <button>
-                        payment page
-                    </button>
-                </Link>
+                
         </div>
     )
 }
