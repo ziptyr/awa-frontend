d61c71ef3b2f13730be5b2c8a908936dbd0f4f99
ziptyr <>
2021-12-25 22:14:55 +0200

    styling changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 import { Outlet } from 'react-router-dom';
 
@@ -18,5 +18,5 @@ export default function Main(props) {
         <Outlet />
       </main>
     </div>
-  )
+  );
 }
