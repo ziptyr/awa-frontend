af375b0f1b5ad7cf4caa193eb7638b4edc310c98
ziptyr <>
2021-12-26 00:54:41 +0200

    Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
+
+import Constants from './Constants.json';
+import { useData } from './DataProvider';
+
+import styles from './Login.module.css';
+
+
+function sendLogin(jwtHeaders, username, password) {
+  axios.post(
+      Constants.API_HEADERS + '/login',
+      {
+        'auth': {
+          'username': username,
+          'password': password}
+      },
+      jwtHeaders
+    )
+
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .then(() => {
+      console.log('login');
+    })
+}
 
 
 export default function Login() {
 
+  const { jwtHeaders } = useData();
+
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   return (
-    <div>
-      Login
-    </div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.line}>
+          <div>
+            Username
+          </div>
+
+          <div>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+          </div>
+        </div>
+
+        <div className={styles.line}>
+          <div>
+            Password
+          </div>
+
+          <div>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </div>
+        </div>
+      </div>
+
+      <button onClick={() => sendLogin(username, password)}>
+        Login
+      </button>
+    </>
   );
 }
